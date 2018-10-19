1e3490d29a43fbd974f01ea3be43078a189deb40
Carlos C Soto <eclipxe13@gmail.com>
2018-10-18 19:56:41 -0500

    Add special test to download without verify peer

--- tests/XmlResourceRetrieverTests/Downloader/PhpDownloaderTest.php
+++ tests/XmlResourceRetrieverTests/Downloader/PhpDownloaderTest.php
@@ -29,4 +29,22 @@ class PhpDownloaderTest extends TestCase
         $this->expectExceptionMessage('Provided context is not a resource');
         $downloader->setContext($isNotResource);
     }
+
+    /**
+     * This test is made to check if can download from https://rdc.sat.gob.mx/ since it is
+     * failing from Sept/2018 delivering an expired certificate
+     */
+    public function testDownloadFromRdcSatGobMxInsecure()
+    {
+        $url = 'https://rdc.sat.gob.mx/rccf/000010/000004/06/25/80/00001000000406258094.cer';
+        $downloader = new PhpDownloader();
+        $context = stream_context_create([
+            'ssl' => ['verify_peer' => false],
+        ]);
+        $downloader->setContext($context);
+        $downloadPath = tempnam('', '') ?: (__DIR__ . '/deleteme.txt');
+        $downloader->downloadTo($url, $downloadPath);
+        $this->assertContains('00001000000406258094', file_get_contents($downloadPath));
+        unlink($downloadPath);
+    }
 }
